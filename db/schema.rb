a3ede23b81e7110e3ad221f53e994b2df03567a1
alex-lemberger <alex.lemberger@gmx.de>
2017-02-15 03:02:25 +0100

    Added Tasks States

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214190646) do
+ActiveRecord::Schema.define(version: 20170215013738) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.         "user"
     t.integer  "user_id"
+    t.string   "state",      default: "to_do"
   end
 
   create_table "users", force: :cascade do |t|
