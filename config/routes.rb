9009fcbfd0f10a184316bc5d8cc55f60340fe9fd
alex-lemberger <alex.lemberger@gmx.de>
2017-02-15 17:42:22 +0100

    Added BS Panels with divs

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   
   get 'contact' => "pages#contact"
 
+  get 'test' => "pages#test"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
